7ea5a44bd64735592c0868d9eb45195ad7909e05
NadiaPia <206i417@gmail.com>
2022-08-21 19:33:35 +0000

    query population logic of buyersimplemented

--- database.js
+++ database.js
@@ -20,3 +20,27 @@ const getIsSold = function(isSold) {
     });
 }
 exports.getIsSold = getIsSold;
+
+const getAllProducts = function() {
+  return pool.query(`SELECT * FROM products;`)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+exports.getAllProducts = getAllProducts;
+
+const getBuyersProducts = function() {
+  return pool.query(`SELECT products.*, users.email, users.user_name FROM products
+  JOIN users ON users.id = seller_id;`)
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  
+}
+exports.getBuyersProducts = getBuyersProducts;
