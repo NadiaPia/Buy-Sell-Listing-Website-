13f3f67c005a7ddd474bcf7223f3cf9af0f5670b
hopeVaughn <hopevaughnwarren@gmail.com>
2022-08-22 16:15:15 -0700

    add more code for route

--- routes/buyers.js
+++ routes/buyers.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    db.getBuyersProducts()
+    db.query(`SELECT products.*, users.email, users.user_name FROM products
+  JOIN users ON users.id = seller_id;`)
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
       .then(products => {
-        console.log(products);
         const templateVars = { cards: products };
-        res.render('buyers', templateVars);
+        res.render('buyers', templateVars)
       })
   });
 
