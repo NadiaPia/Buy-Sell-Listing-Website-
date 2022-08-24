55cde09cb629f3fa79573a03b870408f9e551c1c
hopeVaughn <hopevaughnwarren@gmail.com>
2022-08-24 13:07:19 -0700

    add features to sellers route and small db changes

--- routes/sellers.js
+++ routes/sellers.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { filterProducts } = require('../database');
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    db.query(`SELECT products.*, users.email, users.user_name FROM products
+    db.query(`SELECT products.*, users.email, users.user_name,
+              CASE
+                WHEN products.id in (SELECT products_id FROM favorites WHERE users_id = 2)
+                THEN true
+                ELSE false
+                END AS is_favorite
+                FROM products
               JOIN users ON users.id = seller_id
               LIMIT 12;`)
       .then((result) => {
@@ -18,6 +25,19 @@ module.exports = (db) => {
         res.render('sellers', templateVars)
       })
   });
+
+  router.post('/filter', (req, res) => {
+    const available = req.body.available;
+    const city = req.body.city;
+    const minprice = req.body.minprice;
+    const maxprice = req.body.maxprice;
+    filterProducts(city, minprice, maxprice, available).then((data) => {
+      console.log(data);
+      const templateVars = { products: data }
+      res.render('search', templateVars)
+
+    })
+  })
 
   return router;
 };
