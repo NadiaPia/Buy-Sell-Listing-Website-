e5f7607ea94d451ecff01454407b6a171697fce4
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-08-26 17:37:00 +0000

    fixed contact

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const db = new Pool(dbParams);
 db.connect();
 
 const filterProducts = function(city, minprice, maxprice) {
-  let initialQuery = 'SELECT * FROM products'
+  let initialQuery = 'SELECT products.*, users.email FROM products JOIN users ON users.id = seller_id'
   let queryArr = [];
 
   if (city) {
