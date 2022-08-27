f4e8bf28138c35ca254f3603ed9a230c9c7656c3
hopeVaughn <hopevaughnwarren@gmail.com>
2022-08-27 09:32:45 -0700

    change name of artist in db

--- db/seeds/01_users_seed.sql
+++ db/seeds/01_users_seed.sql
@@ -1,5 +1,5 @@
 insert into users (user_name, email, profile_pic, bio, seller) values ('artbro', 'ftimblettd@scientificamerican.com', 'http://dummyimage.com/156x100.png/dddddd/000000', 'Polarised explicit methodology', false);
-insert into users (user_name, email, profile_pic, bio, seller) values ('jaba-wookie', 'jhrinchishin2@1688.com', 'http://dummyimage.com/229x100.png/dddddd/000000', 'Progressive zero administration circuit', true);
+insert into users (user_name, email, profile_pic, bio, seller) values ('HopeacusMaximus', 'jhrinchishin2@1688.com', 'http://dummyimage.com/229x100.png/dddddd/000000', 'Progressive zero administration circuit', true);
 insert into users (user_name, email, profile_pic, bio, seller) values ('vincentVanGoGo', 'hthiem4@hatena.ne.jp', 'http://dummyimage.com/168x100.png/ff4444/ffffff', 'Integrated modular initiative', true);
 insert into users (user_name, email, profile_pic, bio, seller) values ('MonetMoneyMonet', 'gmeddick7@goodreads.com', 'http://dummyimage.com/100x100.png/cc0000/ffffff', 'Decentralized executive moratorium', true);
 insert into users (user_name, email, profile_pic, bio, seller) values ('aIwannamakeArt', 'mromagosa9@ustream.tv', 'http://dummyimage.com/242x100.png/ff4444/ffffff', 'Down-sized clear-thinking concept', true);
